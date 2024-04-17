6e0e3dfd22a188923746493a0fc90000efd89c48
zulfekar <17052721+ZS25@users.noreply.github.com>
2024-04-17 18:26:17 -0400

    First commit

---
+++ src/main/java/com/sooruth/zuniversityapigateway/ZuniversityApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.sooruth.zuniversityapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZuniversityApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuniversityApiGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sooruth/zuniversityapigateway/ZuniversityApiGatewayApplicationTests.java
@@ -0,0 +1,12 @@
+package com.sooruth.zuniversityapigateway;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuniversityApiGatewayApplicationTests {
+
+/*	@Test
+	void contextLoads() {
+	}*/
+
+}
